2a99c883d09ed4ac04430ddf5a0b36b89e3d567b
YaoXin <2222122099@qq.com>
2020-05-07 09:21:01 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,6 +12,11 @@ use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -19,13 +24,14 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
         //dd($request->avatar);
-
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
