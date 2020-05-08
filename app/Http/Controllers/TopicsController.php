83adfa158cef8c40d07c11e4e35b44c9c9fce97d
YaoXin <2222122099@qq.com>
2020-05-08 07:18:44 +0000

    修复 N+1 问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,7 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		$topics = Topic::with('user', 'category')->paginate(50);
 		return view('topics.index', compact('topics'));
 	}
 
