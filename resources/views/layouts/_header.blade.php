0cb4ad58ee510539034050dd6a6de3ac552d97cc
YaoXin <2222122099@qq.com>
2020-05-13 10:37:16 +0000

    消息通知列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -31,6 +31,13 @@
               <i class="fa fa-plus"></i>
             </a>
           </li>
+
+          <li class="nav-item notification-badge">
+            <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white" href="{{ route('notifications.index') }}">
+              {{ Auth::user()->notification_count }}
+            </a>
+          </li>
+
           <li class="nav-item dropdown">
 
         <li class="nav-item dropdown">
