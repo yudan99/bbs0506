60d7ff6bc11159b04e6ef45150703a8255118338
YaoXin <2222122099@qq.com>
2020-05-13 15:04:02 +0000

    集成 laravel-permission 扩展包

--- database/seeds/RepliesTableSeeder.php
+++ database/seeds/RepliesTableSeeder.php
@@ -4,7 +4,11 @@ use App\Models\Reply;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReplyRequest;
-use Auth;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Seeder;
+use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
 
 
 
